c3c760905e2ae545750ca44f9f1288a5446ae204
patel-param-09 <patelparam9304@gmail.com>
2024-10-09 12:55:55 +0530

    Complete Sidebar Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SideBar from "./components/SideBar";
 
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex ">
+        <SideBar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
